ae217cd93d40d2e0621abfc8eca203ba857f8cd5
Shlammy <shlumhum@outlook.com>
2023-03-06 15:28:00 -0500

    Win/loss/Default message now reflects choices made, playerSelection now case-insesitive

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function getComputerChoice() {
-    const computerChoices = ["Rock", "Paper", "Scissors"];
+    const computerChoices = ["rock", "paper", "scissors"];
     let arrayLength = computerChoices.length;
     let randomInt = Math.random() * arrayLength;
     let randomChoiceIndex = Math.floor(randomInt);
@@ -7,12 +7,12 @@ function getComputerChoice() {
 }
 
 function singleRound(computerSelection, playerSelection) {
-   return (playerSelection == computerSelection) ? 'Draw! Play again.'
-        : (computerSelection == 'Rock') && (playerSelection == 'Paper') ? 'You win! Keep going.'
-        : (computerSelection == 'Rock') && (playerSelection == 'Scissors') ? 'You lose! Play again.'
-        : (computerSelection == 'Paper') && (playerSelection == 'Scissors') ? 'You win! Keep going.'
-        : (computerSelection == 'Paper') && (playerSelection == 'Rock') ? 'You lose! Play again.'
-        : (computerSelection == 'Scissors') && (playerSelection == 'Rock') ? 'You win! Keep going.'
-        : (computerSelection == 'Scissors') && (playerSelection == 'Paper') ? 'You lose! Play again.'
-        : 'Pick Rock, Paper, or Scissors';
+   return (playerSelection.toLowerCase() == computerSelection) ? 'Draw! Play again.'
+        : (computerSelection == 'rock') && (playerSelection.toLowerCase() == 'paper') ? 'You win! Paper beats Rock!'
+        : (computerSelection == 'rock') && (playerSelection.toLowerCase() == 'scissors') ? 'You lose! Rock beats Scissors'
+        : (computerSelection == 'paper') && (playerSelection.toLowerCase() == 'scissors') ? 'You win! Scissors beat Paper'
+        : (computerSelection == 'paper') && (playerSelection.toLowerCase() == 'rock') ? 'You lose! Paper beats Rock'
+        : (computerSelection == 'scissors') && (playerSelection.toLowerCase() == 'rock') ? 'You win! Rock beats Scissors!'
+        : (computerSelection == 'scissors') && (playerSelection.toLowerCase() == 'paper') ? 'You lose! Scissors beat Paper!'
+        : 'You lose! Out of bounds. Pick Rock, Paper, or Scissors';
 }
