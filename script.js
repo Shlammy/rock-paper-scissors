f5966fedad1011f0c8eab5128c62c94b284ba2c5
Shlammy <shlumhum@outlook.com>
2023-03-06 14:31:53 -0500

    singleRound function, compares computerSelection/playerSelection and outputs appropriate win, loss, or default message

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ function getComputerChoice() {
     let randomChoiceIndex = Math.floor(randomInt);
     return computerChoices[randomChoiceIndex];
 }
+
+function singleRound(computerSelection, playerSelection) {
+   return (playerSelection == computerSelection) ? 'Draw! Play again.'
+        : (computerSelection == 'Rock') && (playerSelection == 'Paper') ? 'You win! Keep going.'
+        : (computerSelection == 'Rock') && (playerSelection == 'Scissors') ? 'You lose! Play again.'
+        : (computerSelection == 'Paper') && (playerSelection == 'Scissors') ? 'You win! Keep going.'
+        : (computerSelection == 'Paper') && (playerSelection == 'Rock') ? 'You lose! Play again.'
+        : (computerSelection == 'Scissors') && (playerSelection == 'Rock') ? 'You win! Keep going.'
+        : (computerSelection == 'Scissors') && (playerSelection == 'Paper') ? 'You lose! Play again.'
+        : 'Pick Rock, Paper, or Scissors';
+}
